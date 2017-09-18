257b8a16c7474b26936dd906d75ebdfb357304e9
hackljf <jason.hackl@ufl.edu>
2017-09-18 16:59:02 -0400

    check for zero size in profcat

--- profcat.py
+++ profcat.py
@@ -8,12 +8,13 @@ def profcat(prefix,ncol,nx1,nfiles):
    for p in range(nfiles):
       zefile=prefix+str(p).zfill(ipad)
       if os.access(zefile,os.F_OK):
-         tmp=np.loadtxt(zefile)
-         ntmp=ntmp+1
-         if ntmp==1:
-            fullprof=tmp
-         else:
-            fullprof=np.vstack((fullprof,tmp))
+         if (os.stat(zefile).st_size>0):
+            tmp=np.loadtxt(zefile)
+            ntmp=ntmp+1
+            if ntmp==1:
+               fullprof=tmp
+            else:
+               fullprof=np.vstack((fullprof,tmp))
    fullprof=fullprof.reshape((-1,nx1,ncol))
    vertices=fullprof[:,0,0]
    fullprof=fullprof[vertices.argsort(),:,:].reshape((-1,ncol))
