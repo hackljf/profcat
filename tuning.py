47d06fa87526372b97dd7b7aa1c544f1842b5864
hackljf <jason.hackl@ufl.edu>
2017-09-21 10:33:32 -0400

    helpful, but a bit pyrrhic

--- tuning.py
+++ tuning.py
@@ -1,11 +1,11 @@
 from matplotlib import pyplot as plt
 import tables
-files=["ce40cmax0.3meshh/profiles/prof33611.h5",
-"ce40cmax0.5meshh/profiles/prof99999.h5",
-"ce45cmax0.5messh/profiles/prof99999.h5",
+files=["ce45cmax0.5messh/profiles/prof99999.h5",
 "p12/ce40cmax1isc0.75/prof350000.h5",
 "p12/profiles/prof350000.h5",
 "ce60cmax1isc75meshh/profiles/prof035000.h5"]
+# t<0.035 "ce40cmax0.3meshh/profiles/prof33611.h5",
+# t=0.05, not 0.035 "ce40cmax0.5meshh/profiles/prof99999.h5",
 
 plt.figure()
 for fname in files:
@@ -13,29 +13,45 @@ for fname in files:
    g=[];
    for group in fid.walk_groups():
       g.append(group)
-   profiles=g[0].profiles
-   x=g[0].x.read()
-   hdr=profiles._v_attrs
-   u=profiles.cmt.u.read()
-   T=profiles.cmt.T.read()
-   plt.plot(x,u)
 
+   root=g[0]
+   prof=root.profiles
+   x=root.x.read()
+
+   hdr=prof._v_attrs
+   N=hdr.nx1-1
+
+# there has got to be a better way of doing this. dictionary view?
+   if hdr.__contains__('time'):
+      t='%.2e' % hdr.time
+   else:
+      t='???'
+
+   param=prof.cmt._v_attrs
+   if param.__contains__('CFL'):
+      step=', CFL= %.1f' % param.cfl
+   elif param.__contains__('dt'):
+      step=', $\Delta t$= %.1e' % param.dt
+   else:
+      step=''
+   if param.__contains__('cmax'):
+      cmax=', $c_{max}$= %.1e' % param.cmax
+   else:
+      cmax=''
+   if param.__contains__('ce'):
+      ce=', $c_{E}$= %.1e' % param.ce
+   else:
+      ce=''
+   if param.__contains__('isc'):
+      isc=', $\mathscr{P}$= %.1e' % param.isc
+   else:
+      isc=''
+
+#  u=prof.cmt.u.read()
+   T=prof.cmt.T.read()
+
+   zelab='N ='+str(N)+', t='+t+step+cmax+ce+isc
+   plt.plot(x,T,label=zelab)
+
+plt.legend()
 plt.show()
-import tables
-fid=tables.open_file("ce60cmax1isc75meshh/profiles/prof035000.h5",'r')
-for group in fid.walk_groups():
-    groups.append(group)
-groups[0]
-groups[1]
-prof=groups[0].profiles
-prof
-prof._v_attrs
-g[0].x
-groups[0].x.read()
-%history
-bob=prof._v_attrs
-bob
-bob.dtype
-bob.__contains__('gamma')
-bob.__contains__('CFL')
-bob.__contains__('dt')
