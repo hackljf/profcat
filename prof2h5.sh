783021719be8970366b608729f637855adf11815
hackljf <jason.hackl@ufl.edu>
2017-10-03 16:48:16 -0400

    finally got sed working

--- prof2h5.sh
+++ prof2h5.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 orders="4 8 12 16"
 nelms="50 100 200"
+prof2h5src="/home/local/UFAD/jason.hackl/Work/run_nek_examples/shocktube/data4paper/sod_profiles_logs/prof2h5.py"
 
 for p in $orders
 do
@@ -11,10 +12,10 @@ do
       dir=N${k}_p$p
       cd $dir
 #       grep "<u" *log* > umax
-      cp ../prof2h5.py .
+      cp $prof2h5src .
       spat='s/Np=[0-9]*/Np='$np'/'
       spat2='s/nxd=[0-9]*/nxd='$nxd'/'
-      sed -i -e "'"$spat"'" prof2h5.py
+      sed -i -e $spat -e $spat2 prof2h5.py
       cd ..
    done
 done
