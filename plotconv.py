6848c859dafabc5eef2e1fc708adc660d5b5645f
hackljf <jason.hackl@ufl.edu>
2017-10-13 12:45:32 -0400

    more functions. code is getting better

--- plotconv.py
+++ plotconv.py
@@ -7,83 +7,80 @@ import tables
 from mpl_toolkits.axes_grid.inset_locator import(inset_axes,InsetPosition,mark_inset)
 from matplotlib.ticker import FormatStrFormatter
 
+def plotalot(ax,x,y,xlim,ylim,xticks=None,yticks=None,lab=None):#,fsize)
+   ax.plot(x,y,label=lab)
+   ax.set_xlim(xlim)
+   ax.set_ylim(ylim)
+   if xticks is not None:
+      ax.set_xticks(xticks)
+   if yticks is not None:
+      ax.set_yticks(yticks)
 
-orders=[4,8,12,16]
+orders=[4,8,12]
 mks=['.','x','v','None']
-nelms=[50,100,200]
-h=[0.02,0.01,0.005]
-# one in h, one in p
 width=8.5
 height=5
 fsize=18
 
-exact=np.loadtxt("e1rpex.out",usecols=(0,1))
+exact=np.loadtxt("e1rpex.out",usecols=(0,1)) # rho
+#exact=np.loadtxt("e1rpex.out",usecols=(0,2)) #u
 
+h=0.01
+plt.figure(figsize=(width,height))
+main=plt.axes() # FIDELIUM ANIMAE de whoever gave this SANE DEFAULTS!!!!
+#i1=plt.axes([0.35,0.55,0.5,0.3]) #u
+i1=plt.axes([0.3,0.17,0.32,0.22]) #rho, slow-shock
+i2=plt.axes([0.20,0.5,0.25,0.32]) #rho, contact
 for i,p in enumerate(orders):
-   plt.figure(figsize=(width,height))
-   main=plt.axes() # FIDELIUM ANIMAE de whoever gave this SANE DEFAULTS!!!!
-#  i1=plt.axes([0.4,0.175,0.33,0.5]) #u
-   i1=plt.axes([0.43,0.43,0.44,0.44]) #rho
-   for k,K in enumerate(nelms):
-      case='N'+str(K)+'_p'+str(p)
-      zefile=case+'/profiles/prof020001.h5'
-      print zefile
-      if os.access(zefile,os.F_OK):
-         if (os.stat(zefile).st_size>0):
-            fid=tables.open_file(zefile,'r')
-            g=[];
-            for group in fid.walk_groups():
-               g.append(group)
-            root=g[0]
-            prof=root.profiles
-            x=root.x.read()
-            hdr=prof._v_attrs
-            N=hdr.nx1-1
-   
-# there has got to be a better way of doing this. dictionary view?
-#           if hdr.__contains__('time'):
-#              t=hdr.time
-         
-            param=prof.cmt._v_attrs
-            if param.__contains__('isc'):
-               ipr=param.isc
-            else:
-               ipr=1
+   case='h0.01/p'+str(p).zfill(2)
+   zefile=case+'/profiles/prof035000.h5'
+   print zefile
+   if os.access(zefile,os.F_OK):
+      if (os.stat(zefile).st_size>0):
+         fid=tables.open_file(zefile,'r')
+         prof=fid.root.profiles
+         x=fid.root.x.read()
+         x=x+0.4 # diaphragm
+         hdr=prof._v_attrs
+         print p-(hdr.nx1-1)
 
-            u=prof.cmt.u.read()
-            T=prof.cmt.T.read()
-            rho=prof.cmt.d.read()
-            mumax=prof.cmt.mumax.read()
-            nus=prof.cmt.artdiff.read()
-            mu=nus/ipr
-#           zelab='p ='+str(N)+', t='+t+step+cmax+ce+isc
-            zelab='h ='+str(h[k])
-#           main.plot(x,u,label=zelab)
-            main.plot(x,rho,label=zelab)
-#           main.plot(x,T,label=zelab)
-            main.set_xlabel(r'$x$',fontsize=fsize)
-#           main.set_ylabel(r'$u$',fontsize=fsize)
-            main.set_ylabel(r'$\rho$',fontsize=fsize)
-            main.set_xlim((0.2,1.0)) # need to start storing and
-#           i1.set_xlim((0.45,0.85)) # u need to start storing and
-            i1.set_xlim((0.5,0.675)) # u need to start storing and
-#           i1.set_ylim((0.926,0.9325))# u func-ifying these xlims
-            i1.set_ylim((0.424,0.428))# rho func-ifying these xlims
-#           i1.set_xticks([0.5,0.6,0.7,0.8]) #u
-            i1.set_xticks([0.5,0.55,0.6,0.65]) #rho
-#           i1.set_yticks([0.926,0.928,0.93,0.932]) #u
-            i1.set_yticks([0.424,0.425,0.426,0.427,0.428]) #rho
-#           i1.plot(x,u)
-            i1.plot(x,rho)
+         param=prof.cmt._v_attrs
+         if param.__contains__('isc'):
+            ipr=param.isc
+         else:
+            ipr=1
 
-            fid.close()
+         u=prof.cmt.u.read()
+         print u.shape
+         T=prof.cmt.T.read()
+         rho=prof.cmt.d.read()
+         mumax=prof.cmt.mumax.read()
+         nus=prof.cmt.artdiff.read()
+         mu=nus/ipr
+         zelab='p ='+str(p)
+#        plotalot(main,x,rho,(0.35,1.05),(-10.0,35.0),lab=zelab)
+         plotalot(main,x,rho,(0.35,1.05),(0.0,35.0),lab=zelab)
+         main.set_xlabel(r'$x$',fontsize=fsize)
+#        main.set_ylabel(r'$u$',fontsize=fsize)
+         main.set_ylabel(r'$\rho$',fontsize=fsize)
+#        plotalot(i1,x,rho,(0.4,0.75),(14.0,14.6),xticks=[0.4,0.5,0.6,0.7],yticks=[14.0,14.2,14.4,14.6])
+         plotalot(i1,x,rho,(0.4,0.75),(14.2,14.4),xticks=[0.4,0.5,0.6,0.7],yticks=[14.2,14.3,14.4])
+         plotalot(i2,x,rho,(0.7,0.85),(30.6,31.2),xticks=[0.7,0.75,0.8,0.85],yticks=[30.6,30.8,31.0,31.2])
+#        i1.set_xlim((0.4,0.85)) # u need to start storing and
+#        i1.set_ylim((8.66,8.72))# u func-ifying these xlims
+#        i1.set_xticks([0.5,0.6,0.7,0.8]) #u
+#        i1.set_yticks([8.66,8.68,8.70,8.72]) #u
+#        i1.plot(x,u)
+
+         fid.close()
 
 #  main.legend(loc='upper left') #u
-   main.plot(exact[:,0],exact[:,1],label='exact')
-   i1.plot(exact[:,0],exact[:,1])
-   main.legend(loc='lower left')
+main.plot(exact[:,0],exact[:,1],label="exact",ls='--')
+i1.plot(exact[:,0],exact[:,1],ls='--')
+i2.plot(exact[:,0],exact[:,1],ls='--')
+#main.legend(loc='lower left') #u
+main.legend(loc='upper right') #rho
 
-#  pname='sod3_u_t02_p'+str(p)+'.pdf'
-   pname='sod3_rho_t02_p'+str(p)+'.pdf'
-   plt.savefig(pname)
-   plt.close()
+pname='shock2_d_t02_K100.pdf'
+plt.savefig(pname)
+plt.close()
--- plotconv_noinset.py
+++ plotconv_noinset.py
@@ -8,62 +8,66 @@ from mpl_toolkits.axes_grid.inset_locator import(inset_axes,InsetPosition,mark_i
 from matplotlib.ticker import FormatStrFormatter
 
 
-orders=[4,8,12,16]
 mks=['.','x','v','None']
-nelms=[50,100,200]
 h=[0.02,0.01,0.005]
 # one in h, one in p
 width=8.5
 height=5
 fsize=18
 
-exact=np.loadtxt("e1rpex.out",usecols=(0,2))
+#exact=np.loadtxt("e1rpex.out",usecols=(0,2)) # u
+exact=np.loadtxt("e1rpex.out",usecols=(0,1)) # rho
 
-for i,p in enumerate(orders):
-   plt.figure(figsize=(width,height))
-   main=plt.axes() # FIDELIUM ANIMAE de whoever gave this SANE DEFAULTS!!!!
-   for k,K in enumerate(nelms):
-      case='N'+str(K)+'_p'+str(p)
-      zefile=case+'/profiles/prof020001.h5'
-      print zefile
-      if os.access(zefile,os.F_OK):
-         if (os.stat(zefile).st_size>0):
-            fid=tables.open_file(zefile,'r')
-            g=[];
-            for group in fid.walk_groups():
-               g.append(group)
-            root=g[0]
-            prof=root.profiles
-            x=root.x.read()
-            hdr=prof._v_attrs
-            N=hdr.nx1-1
-   
+p=8
+plt.figure()#figsize=(width,height))
+main=plt.axes() # FIDELIUM ANIMAE de whoever gave this SANE DEFAULrhoS!!!!
+for k,K in enumerate(h):
+   case='h'+str(K)+'/p'+str(p).zfill(2)
+   zefile=case+'/profiles/prof035000.h5'
+   print zefile
+   if os.access(zefile,os.F_OK):
+      if (os.stat(zefile).st_size>0):
+         fid=tables.open_file(zefile,'r')
+         prof=fid.root.profiles
+         x=fid.root.x.read()
+         x=x+0.4
+         hdr=prof._v_attrs
+         print p-(hdr.nx1-1)
+
 # there has got to be a better way of doing this. dictionary view?
 #           if hdr.__contains__('time'):
 #              t=hdr.time
-         
-            param=prof.cmt._v_attrs
-            if param.__contains__('isc'):
-               ipr=param.isc
-            else:
-               ipr=1
+      
+         param=prof.cmt._v_attrs
+         if param.__contains__('isc'):
+            ipr=param.isc
+         else:
+            ipr=1
 
-            u=prof.cmt.u.read()
-#           mumax=prof.cmt.mumax.read()
-#           nus=prof.cmt.artdiff.read()
-#           mu=nus/ipr
-#           zelab='p ='+str(N)+', t='+t+step+cmax+ce+isc
-            zelab='h ='+str(h[k])
-            main.plot(x,u,label=zelab)
-            main.set_xlabel(r'$x$',fontsize=fsize)
-            main.set_ylabel(r'$u$',fontsize=fsize)
-            main.set_xlim((0.2,1.0)) # need to start storing and
+#        u=prof.cmt.u.read()
+         rho=prof.cmt.d.read()
+#        mumax=prof.cmt.mumax.read()
+#        nus=prof.cmt.artdiff.read()
+#        mu=nus/ipr
+#        zelab='p ='+str(N)+', t='+t+step+cmax+ce+isc
+         zelab='h ='+str(K)
+#        main.plot(x,u,label=zelab)
+         main.plot(x,rho,label=zelab)
+         main.set_xlabel(r'$x$',fontsize=fsize)
+#        main.set_ylabel(r'$u$',fontsize=fsize)
+         main.set_ylabel(r'$\rho$',fontsize=fsize)
+#        main.set_xlim((0.35,1.0)) # need to start storing and
+# need to change legend size?
+         main.set_xlim((0.35,1.05)) # need to start storing and
+         main.set_ylim((0.0,35.0)) # need to start storing and
+         fid.close()
 
-            fid.close()
+main.plot(exact[:,0],exact[:,1],label="exact",ls='--')
+#main.legend(loc='lower left') #u
+main.legend(loc='upper right') #u
 
-   main.plot(exact[:,0],exact[:,1],label="exact")
-   main.legend(loc='upper left') #u
-
-   pname='sod3_u_t02_p'+str(p)+'noinset.pdf'
-   plt.savefig(pname)
-   plt.close()
+pname='shock2_rho_t35_p'+str(p)+'noinset.pdf'
+#pname='shock2_u_t35_p'+str(p)+'noinset.pdf'
+plt.savefig(pname)
+plt.close()
+#plt.show()
