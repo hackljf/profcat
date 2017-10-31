2d089011cbe0f7c3c7fcadf40b9994335597c833
hackljf <jason.hackl@ufl.edu>
2017-10-31 17:14:15 -0400

    now with final table data in EOCshock2.py

--- EOCshock2.py
+++ EOCshock2.py
@@ -8,22 +8,31 @@ pconv=pconv[1:]
 hconv=np.loadtxt("conv",skiprows=2)
 hconv=hconv[0,1:]
 h=[0.02,0.01,0.005]
-p=[4,8,12]
+orders=[4,8,12]
 #del m_hconv m_pconv b_hconv b_pconv
 m_hconv, b_hconv= np.polyfit(np.log10(h),np.log10(hconv),1)
-m_pconv, b_pconv= np.polyfit(np.log10(p),np.log10(pconv),1)
+m_pconv, b_pconv= np.polyfit(np.log10(orders),np.log10(pconv),1)
 plt.loglog(h,h**m_hconv*(10**b_hconv),'r-',h,hconv,'bo-')
-print m_hconv
+print m_hconv #0.996033980523
 plt.xlabel(r'$h$',fontsize=fsize)
 plt.tick_params(axis='both',labelsize=fsize,length=7,width=1.2)
 plt.ylabel(r'$L_1(E(\langle\rho\rangle_{\Omega_e}))$',fontsize=fsize)
 plt.tight_layout()
 plt.savefig('shock2_rho_hconv_p8.pdf')                                                                             
 plt.close()
-print m_pconv
-plt.loglog(p,p**m_pconv*(10**b_pconv)*1.1,'r-',p,pconv,'bo-')
+print m_pconv # -1.07797773187
+plt.loglog(orders,orders**m_pconv*(10**b_pconv)*1.1,'r-',orders,pconv,'bo-')
 plt.xlabel(r'$N-1$',fontsize=fsize)
 plt.tick_params(axis='both',labelsize=fsize,length=7,width=1.2)
 plt.ylabel(r'$L_1(E(\langle\rho\rangle_{\Omega_e}))$',fontsize=fsize)
 plt.tight_layout()
 plt.savefig('shock2_rho_pconv_K100.pdf')                                                                             
+
+p=8
+#   m_hconv, b_hconv= np.polyfit(np.log10(spacings),np.log10(err[i,:]),1)
+#   EOCh.append(m_hconv)
+print hconv #[ 0.75354069  0.2458658   0.18942378]
+localdiff=np.divide(np.diff(np.log10(hconv)),np.diff(np.log10(h)))
+print h # [0.02, 0.01, 0.005]
+print hconv  #[ 0.75354069  0.2458658   0.18942378]
+print localdiff #[ 1.61581435  0.37625361]
